5b73142fa6a19e8969b8a527d526a2e62c38063c
Marina Monsivais <68879755+MinaMonsi@users.noreply.github.com>
2021-08-01 19:14:55 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import ItemList from './components/ItemList';
+import './App.css';
+import LoginPage from './components/LoginPage';
+import SignUp from './components/SignUp';
+import PrivateRoute from './components/PrivateRoute'
+import NavBar from './components/NavBar';
+import SearchItems from './components/SearchItems';
+
+function App() {
+
+
+
+  return (
+    <Router>
+    <div className="App">
+    <Router>
+     <header className="App-header">
+        <h1>Use My Tech Stuff</h1>
+        <NavBar/>
+      </header>     
+    
+    <Switch> 
+      <Route exact path='/' component={LoginPage}/>
+      <Route path='/signup' component={SignUp}/>
+      <PrivateRoute path='/itemList' component={ItemList}/>
+      <PrivateRoute path ='/searchItems' component={SearchItems}/>
+    </Switch>
+    </Router>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
